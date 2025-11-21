5fd8809c3d348271bcbee086ba33ad8ca7548ec7
Owusu-Addai Emmanuel <owusuaddaiemmanuel35@gmail.com>
2025-11-21 05:33:10 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h2><em>Welcome To OWASP ZAP DAST web app</em></h2>");
+});
+
+// Intentional insecure endpoint
+app.get("/search", (req, res) => {
+  const q = req.query.q;
+  res.send("Search term: " + q);
+});
+
+app.listen(3000, () => console.log("App running on http://localhost:3000"));
+
